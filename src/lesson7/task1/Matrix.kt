69c692b4be3c938d060c0641ae368c55fede2b91
vdonikai <nikitavdovenko60@gmail.com>
2017-12-24 20:00:47 +0300

    Урок 7, тестовая матрица для случайных тестов котоеда

--- src/lesson7/task1/Matrix.kt
+++ src/lesson7/task1/Matrix.kt
@@ -38,32 +38,55 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0) throw IllegalArgumentException()
+    return MatrixImpl(height, width, e)
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
+    private val map = mutableMapOf<Cell, E>()
 
-    override val width: Int = TODO()
+    override fun get(row: Int, column: Int): E = map[Cell(row, column)]!!
 
-    override fun get(row: Int, column: Int): E  = TODO()
-
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(cell: Cell): E  = map[cell]!!
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        map[Cell(row, column)] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        map[cell] = value
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?) =
+            other is MatrixImpl<*> &&
+            height == other.height &&
+            width == other.width
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append("[")
+        for (row in 0 until height) {
+            sb.append("[")
+            for (column in 0 until width) {
+                sb.append(this[row, column])
+            }
+            sb.append("]")
+        }
+        sb.append("]")
+        return "$sb"
+    }
+
+    override fun hashCode(): Int {
+        var result = 5
+        result = 31 * result + width
+        result = 31 * result + height
+        return result
+    }
 }
 
